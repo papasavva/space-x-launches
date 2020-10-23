653abec2929993079412a2d317623bc0a785509b
papasavvas <papasavva@protonmail.com>
2020-10-23 20:47:21 +0200

    GraphQL Schema and server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { Schema } from "./schema";
+
+const application = express();
+
+application.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: Schema.get(),
+    graphiql: true,
+  })
+);
+
+const PORT = 4000;
+application.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
