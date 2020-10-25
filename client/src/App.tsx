d5503b635560eed9706e5ae41ecdcc9b8816ddc8
papasavvas <papasavva@protonmail.com>
2020-10-25 15:24:41 +0100

    Created react app

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import './bootstrap.min.css'
+import logo from './logo.png'
+
+function App() {
+    return (
+        <div className="App">
+            <img src={logo} alt="SpaceX logo" style={{ width: 500, display: 'block', margin: 'auto' }} />
+        </div>
+    )
+}
+
+export default App
