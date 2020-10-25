3dc982aa822061b5a643ae6f614af6b50b46df83
papasavvas <papasavva@protonmail.com>
2020-10-25 13:05:36 +0100

    Created react app and installed concurrently

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import { Schema } from './schema'
 
-import formatter from './helpers/formatter'
+import { rightPad } from './helpers/formatter'
 const application = express()
 
 application.use(
@@ -15,7 +15,7 @@ application.use(
 
 const PORT = 4000
 application.listen(PORT, () => {
-    console.log(`${formatter.rightPad('Application:')} SpaceX Launches`)
-    console.log(`${formatter.rightPad('GraphQLi:')} http://localhost:4000/graphql`)
-    console.log(`${formatter.rightPad('Status:')} Up and Running 🚀`)
+    console.log(`${rightPad('Application:')} SpaceX Launches`)
+    console.log(`${rightPad('GraphQLi:')} http://localhost:4000/graphql`)
+    console.log(`${rightPad('Status:')} Up and Running 🚀`)
 })
